ac13b42a6a9b6bedf449b36592ea68f504860b26
alexfernandez2000 <alexfernandezromero1@gmail.com>
2024-11-29 13:58:28 +0100

    ViewModel creation

---
+++ BookSource/BookSource/Models/ViewModel/BookListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BookSource.Models.ViewModel
+{
+    public class BookListViewModel
+    {
+       public List<BookViewModel>? Books {  get; set; }
+    }
+}
---
+++ BookSource/BookSource/Models/ViewModel/BookViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BookSource.Models.ViewModel
+{
+    public class BookViewModel
+    {
+        public required int IdBook { get; set; }
+        public required string Title { get; set; }
+        public string? SubTitle { get; set; }
+        public string? ImageLink { get; set; }
+        public string? Description { get; set; }
+        public required string Editorial { get; set; }
+        public required int PageCount { get; set; }
+        public required double Score { get; set; }
+        public required string Author { get; set; }
+        public required List<CategoryViewModel> Categories { get; set; }    
+    }
+}
