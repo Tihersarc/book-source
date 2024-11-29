9851d743be57a20d738863cd7c035bc853adc776
Pol <tihersarc35@gmail.com>
2024-11-29 10:25:04 +0100

    MVC project

--- BookSource/BookScraper/Scripts/Scraper.cs
+++ BookSource/BookScraper/Scripts/Scraper.cs
@@ -11,9 +11,14 @@ namespace BookScraper.Scripts
     {
         public HttpClient HttpClient { get; private set; }
 
+        private readonly string endpointString = "https://www.googleapis.com/books/v1/volumes?";
+
         public Scraper() 
         {
             HttpClient = new HttpClient();
+
+            HttpClient.BaseAddress = new Uri(endpointString);
+            HttpClient.DefaultRequestHeaders.Accept.
         }
 
         public void GetBooks()
---
+++ BookSource/BookSource/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookSource.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
