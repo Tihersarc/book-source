568995d4821f13228384e8da90147edbbb99edae
Pol <tihersarc35@gmail.com>
2024-11-29 10:17:38 +0100

    Added scraper project

---
+++ BookSource/BookScraper/Scripts/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookScraper.Scripts
+{
+    internal class Book
+    {
+        string Name {  get; set; }
+        string Author { get; set; }
+        string Genre { get; set; }
+        string Descritpion {  get; set; }
+
+        public Book(string name, string author, string genre, string descripton) {
+            Name = name;
+            Author = author;
+            Genre = genre;
+            Descritpion = descripton;
+        }
+    }
+}
---
+++ BookSource/BookScraper/Scripts/Scraper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookScraper.Scripts
+{
+    internal class Scraper
+    {
+        public HttpClient HttpClient { get; private set; }
+
+        public Scraper() 
+        {
+            HttpClient = new HttpClient();
+        }
+
+        public void GetBooks()
+        {
+
+        }
+    }
+}
