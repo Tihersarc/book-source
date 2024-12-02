b40058939a876ed7724fed142673f57965a389f4
alexfernandez2000 <alexfernandezromero1@gmail.com>
2024-12-02 11:10:47 +0100

    BookList partial finished

--- BookSource/BookSource/Tools/Tools.cs
+++ BookSource/BookSource/Tools/Tools.cs
@@ -10,6 +10,8 @@ namespace BookSource.Tools
             bookList.Add(new BookViewModel { Author = "Shiro",Categories = CategoryListTemporal(),Description="La mejor loli del mundo",Editorial="Editorial",IdBook=1, ImageLink="https://i.pinimg.com/736x/92/93/90/929390ff37e0213ff37e2ad46290cd0c--anime-characters-video-game.jpg",PageCount=12,Score=5,SubTitle="NGNL",Title="Shiro" });
             bookList.Add(new BookViewModel { Author = "Kanna", Categories = CategoryListTemporal(), Description = "La tercera mejor loli del mundo", Editorial = "Editorial", IdBook = 2, ImageLink = "https://i.pinimg.com/736x/92/93/90/929390ff37e0213ff37e2ad46290cd0c--anime-characters-video-game.jpg", PageCount = 12, Score = 5, SubTitle = "NGNL", Title = "No game no life" });
             bookList.Add(new BookViewModel { Author = "Izuna", Categories = CategoryListTemporal(), Description = "La segunda mejor loli del mundo", Editorial = "Editorial", IdBook = 3, ImageLink = "https://i.pinimg.com/736x/92/93/90/929390ff37e0213ff37e2ad46290cd0c--anime-characters-video-game.jpg", PageCount = 12, Score = 5, SubTitle = "NGNL", Title = "Izuna" });
+            bookList.Add(new BookViewModel { Author = "Empty", Categories = CategoryListTemporal(),  Editorial = "Empty", IdBook = 4, PageCount = 12, Score = 5, Title = "Empty" });
+
             return bookList;
         }
         public static List<CategoryViewModel> CategoryListTemporal()
