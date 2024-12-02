c3ccfec8d2cd20abf4fb919c3d70febf52a15843
Daniel Alvarez <danipoal.0@gmail.com>
2024-12-02 17:19:36 +0100

    SetUp GetLogin UserDal

---
+++ BookSource/BookSource/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookSource.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BookSource/BookSource/DAL/UserDAL.cs
+++ BookSource/BookSource/DAL/UserDAL.cs
@@ -1,19 +1,42 @@
 ﻿using BookSource.Models;
+using Microsoft;
+using System.Data.SqlClient;
+
 namespace BookSource.DAL
 {
     public class UserDAL
     {
 
         private readonly IConfiguration _configuration;
+        private string connectionString;
         public UserDAL(IConfiguration configuration)
         {
            _configuration = configuration;
-           var connectionString = _configuration.GetConnectionString("DefaultConnection");
+           connectionString = _configuration.GetConnectionString("DefaultConnection");
 
         }
 
-        public User getUsuarioLogin(string username, string password)
+        [Obsolete]
+        public User GetUsuarioLogin(string username, string password)
         {
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM [User] WHERE UserName = @UserName;";
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@UserName", username);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        // If there is a User found, we have to get the paswords
+                        // and verify if the salt hash + password is right
+                    }
+                }
+            }
+
             return null;
         }
     }
