617aea6f1b1271c4694908c5698c9897f3cb4e06
alexFernandez <70752172+alexfernandez2000@users.noreply.github.com>
2024-12-04 22:57:04 +0100

    User structure finished

--- BookSource/BookSource/Controllers/UserController.cs
+++ BookSource/BookSource/Controllers/UserController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookSource.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookSource.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string? username)
         {
+            string? sessionUsername = HttpContext.Session.GetString("Username");
+            ViewBag.Username = sessionUsername;
+
+            if (!string.IsNullOrEmpty(username))
+            {
+                //TODO Get user
+
+                return View(Tools.Tools.UserTemporal());//Enviar el viewModel del usuario obtenido
+            }
+
             return View(Tools.Tools.UserTemporal());
+        }
+        [HttpGet]
+        public IActionResult ChangeList(UserViewModel? user)
+        {
+            return View("Index",user);
         }
     }
 }
