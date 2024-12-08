ee4cfc3a5a8ce571920e329fc04c41cad00fbbe1
alexFernandez <70752172+alexfernandez2000@users.noreply.github.com>
2024-12-08 22:50:56 +0100

    Base userlist of books

---
+++ BookSource/BookSource/Controllers/ListOfBooksController.cs
@@ -0,0 +1,24 @@
+﻿using BookSource.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSource.Controllers
+{
+    public class ListOfBooksController : Controller
+    {
+        public IActionResult Index(string username, int? selectedListId)
+        {
+            UserListOfBooksViewModel userListOfBooksViewModel = new UserListOfBooksViewModel { 
+                UserName = username,
+                IdSelectedList=selectedListId,
+
+            };
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ChangeList(string? username, UserListOfBooksViewModel userList)
+        {
+            return RedirectToAction("Index", "ListOfBooks", new { username = username, selectedListId = userList.IdSelectedList});
+        }
+
+    }
+}
---
+++ BookSource/BookSource/Models/ViewModel/UserListOfBooksViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BookSource.Models.ViewModel
+{
+    public class UserListOfBooksViewModel
+    {
+        public required string UserName { get; set; }
+        public int? IdSelectedList { get; set; }
+
+        public List<ListOfBooksViewModel> UserListOfBooks{get;set;}
+    }
+}
