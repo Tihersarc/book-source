aa10ddc2f51522f8482dce727ac5b143c4e3260b
Daniel Alvarez <danipoal.0@gmail.com>
2024-12-10 16:31:40 +0100

    Filters design first version

---
+++ BookSource/BookSource/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using BookSource.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSource.Controllers
+{
+    public class SearchController : Controller
+    {
+
+        // Recibe un viewModel con los filtros
+        // Devuelve el Book list gestionado
+        [HttpGet("search")]
+        public IActionResult Index(SearchViewModel view)
+        {
+            SearchViewModel viewModel = new SearchViewModel();
+            return View(viewModel);
+
+        }
+    }
+}
