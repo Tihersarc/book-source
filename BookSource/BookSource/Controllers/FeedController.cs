60bfac37a594625859e97f4deb6c9cce38b3317b
Daniel Alvarez <danipoal.0@gmail.com>
2024-12-11 18:26:23 +0100

    SetUp files for feed

---
+++ BookSource/BookSource/Controllers/FeedController.cs
@@ -0,0 +1,13 @@
+﻿using BookSource.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSource.Controllers
+{
+    public class FeedController : Controller
+    {
+        public IActionResult Index(FeedViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
---
+++ BookSource/BookSource/Models/ViewModel/FeedViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BookSource.Models.ViewModel
+{
+    public class FeedViewModel
+    {
+        // To change to Publication View Model
+        public List<Publication>? Publications { get; set; }
+        public string? Content { get; set; }
+        public string? OrderType { get; set; }
+        public User User { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
