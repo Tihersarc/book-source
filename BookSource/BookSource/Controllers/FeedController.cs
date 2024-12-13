28c48ca1cd2d71a696db7e582f63cbca320ab41a
Daniel Alvarez <danipoal.0@gmail.com>
2024-12-13 10:15:34 +0100

    Fix Search navBar + Publication 2nd setUp

--- BookSource/BookSource/Controllers/FeedController.cs
+++ BookSource/BookSource/Controllers/FeedController.cs
@@ -1,13 +1,67 @@
-﻿using BookSource.Models.ViewModel;
+﻿using BookSource.DAL;
+using BookSource.Models;
+using BookSource.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 
 namespace BookSource.Controllers
 {
     public class FeedController : Controller
     {
-        public IActionResult Index(FeedViewModel viewModel)
+        private UserDAL _userDAL;
+        public FeedController(UserDAL userDAL)
         {
-            return View(viewModel);
+            _userDAL = userDAL;
         }
+
+        public IActionResult Index(FeedViewModel model)
+        {
+            model.Users = _userDAL.GetAllUsers();
+            //model.Publications = 
+            return View(model);
+        }
+
+        //public List<Publications?> GetBooksByFilter(SearchViewModel model)
+        //{
+        //    List<Book> books = new List<Book>();
+
+        //    // Obtenemos todos los libros o los de una categoria
+        //    if (model.Category != "" && model.Category != null)
+        //        books = _bookDAL.GetBooksByCategory(model.Category);
+        //    else
+        //        books = _bookDAL.GetAllBooks();
+
+        //    // Si se ha intorducido algo en Title, filtramos por titulo
+        //    if (model.Title != "" && model.Title != null)
+        //        books = books.Where(book => book.Title.Contains(model.Title, StringComparison.OrdinalIgnoreCase)).ToList();
+
+        //    // Ordenar los libros si se ha puesto algo
+        //    if (model.OrderType != null && model.OrderType != "")
+        //    {
+        //        switch (model.OrderType)
+        //        {
+        //            case "MaxScore":
+        //                books = books.OrderByDescending(book => book.Score).ToList();
+        //                break;
+        //            case "MinScore":
+        //                books = books.OrderBy(book => book.Score).ToList();
+        //                break;
+        //            case "MaxPage":
+        //                books = books.OrderByDescending(book => book.PageCount).ToList();
+        //                break;
+        //            case "MinPage":
+        //                books = books.OrderBy(book => book.PageCount).ToList();
+        //                break;
+        //            case "ByAuthor":
+        //                books = books.OrderBy(book => book.Author).ToList();
+        //                break;
+        //            case "ByEditorial":
+        //                books = books.OrderBy(book => book.Editorial).ToList();
+        //                break;
+        //        }
+        //    }
+        //    return books;
+
+        //}
     }
 }
--- BookSource/BookSource/Models/ViewModel/FeedViewModel.cs
+++ BookSource/BookSource/Models/ViewModel/FeedViewModel.cs
@@ -6,7 +6,7 @@
         public List<Publication>? Publications { get; set; }
         public string? Content { get; set; }
         public string? OrderType { get; set; }
-        public User User { get; set; }
-        public List<User> Users { get; set; }
+        public User User { get; set; }      // El usuario seleccionado
+        public List<User> Users { get; set; }   // Todos los users para el combobox
     }
 }
