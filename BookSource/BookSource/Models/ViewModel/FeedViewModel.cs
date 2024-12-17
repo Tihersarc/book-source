053613cd7612c8ef287119f01cf5a3af91953c1a
Pol <tihersarc35@gmail.com>
2024-12-17 12:23:38 +0100

    Fixed filter by user filter. + Added param to FeedViewModel

--- BookSource/BookSource/Models/ViewModel/FeedViewModel.cs
+++ BookSource/BookSource/Models/ViewModel/FeedViewModel.cs
@@ -6,7 +6,8 @@
         public List<Models.ViewModel.PublicationViewModel>? Publications { get; set; }
         public string? Title { get; set; }
         public string? OrderType { get; set; }
-        public User User { get; set; }      // El usuario seleccionado
+        public User? User { get; set; }      // El usuario seleccionado
+        public int? IdUser { get; set; }
         public List<User> Users { get; set; }   // Todos los users para el combobox
         
         public List<PublicationLikes>? PublicationsLikes { get; set; } // Todas las relaciones de likes con users para mapearlas
